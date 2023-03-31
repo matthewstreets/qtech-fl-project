99cff57c260fc25737951219713633033f69a04f
Matthew Streets <87211678+matthewstreets@users.noreply.github.com>
2023-03-31 12:32:08 +0100

    implemented quiz function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,128 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const quizContainer = document.getElementById('quiz-container')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const finalScore = document.getElementById('final-score')
+const quizLength = document.getElementById('quiz-length')
+
+let score = 0
+let shuffledQuestions, currentQuestionIndex 
 
 startButton.addEventListener('click', startQuiz)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++ 
+    setNextQuestion()
+})
 
 function startQuiz() {
-    console.log('started')
     startButton.classList.add('hide')
+    finalScore.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
-
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function chooseAnswer() {
-
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text 
+        button.classList.add('btn')
+        button.classList.add('btn-primary')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 }
+
+function resetState() {
+    clearStatusClass(document.getElementById('body'))
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target 
+    const correct = selectedButton.dataset.correct
+    if (correct) {
+        score++
+    } 
+    setStatusClass(document.getElementById('body'), correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+        if (button.dataset.correct) {
+            button.classList.remove('btn-primary')
+            button.classList.add('btn-success')
+        } else {
+            button.classList.remove('btn-primary')
+            button.classList.add('btn-danger')
+        }
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+        finalScore.classList.remove('hide')
+        quizContainer.classList.add('mb-3')
+        document.getElementById('score').innerHTML = score;
+        document.getElementById('quiz-length').innerHTML = questions.length;
+        score = 0;
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const questions = [
+    {
+        question: '_________ is the branch of knowledge concerned with the production, consumption and transfer of wealth.',
+        answers: [
+            { text: 'economics', correct: true},
+            { text: 'philosophy', correct: false},
+            { text: 'theology', correct: false},
+            { text: 'astronomy', correct: false}
+        ]
+    },
+    {
+        question: "The _______________ affects every aspect of our daily lives, including our finances, job security, and the economy's overall health",
+        answers: [
+            { text: 'political economy', correct: false},
+            { text: 'financial system', correct: true},
+            { text: 'banking sector', correct: false},
+            { text: 'parliamentary government', correct: false}
+        ]
+    },
+    {
+        question: "_____________________ are organisations such as banks, insurance, companies, and investment firms that provide financial services and products to individuals and businesses.",
+        answers: [
+            { text: 'Global forums', correct: false},
+            { text: 'Financial instruments', correct:false},
+            { text: 'Financial markets', correct: false},
+            { text: 'Financial institutions', correct: true}
+        ]
+    }
+]
