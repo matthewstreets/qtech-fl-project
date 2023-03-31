266122749c88770f95a1d29bcb3e8a3d63eff475
Matthew Streets <87211678+matthewstreets@users.noreply.github.com>
2023-03-31 10:19:22 +0100

    starting quiz development

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click', startQuiz)
+
+function startQuiz() {
+    console.log('started')
+    startButton.classList.add('hide')
+}
+
+function setNextQuestion() {
+
+}
+
+function chooseAnswer() {
+
+}
